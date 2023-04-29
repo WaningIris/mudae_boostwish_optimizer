678954e5b4d9c7aa6a0be47db108c5bd0d767136
bog_4t <97702184+bog4t@users.noreply.github.com>
2023-04-29 10:19:15 -0400

    Bug fixes

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import math
 
-base_wb_B = 100     # base badge wish boost
-base_wb_F = 300     # base first wish boost
-wl = 13             # number of wishes in your roll type
-wp = 0              # wish protection rate
-C_D = 16203         # number of disabled characters in your roll type
-C_L = 30967         # number of unclaimed characters in your roll type
-C_T = 31706         # total number of characters in your roll type
-pr = 2              # your personal rare value
-base_n = 22         # number of total rolls
-base_bonus_n = 6    # number of bonus rolls
+base_wb_B = 150     # base badge wish boost
+base_wb_F = 200     # base first wish boost
+wl = 500            # number of wishes in your roll type
+wp = 1/499          # wish protection rate
+C_D = 30708         # number of disabled characters in your roll type
+C_L = 0             # number of unclaimed characters in your roll type
+C_T = 31708         # total number of characters in your roll type
+pr = 1              # your personal rare value
+base_n = 0          # number of total rolls
+base_bonus_n = 1    # number of bonus rolls
 x = 1               # number of wishes you want
 
 
 # Calculates chance of a rolling wish
 def P(wb_B, wb_F):
-    return wp + (wl * (1 + wb_B / 100) + wb_F / 100) / (C_L - C_D + ((1 - C_L / C_T) ** pr) * C_T)
+    return wp + (wl * (1 + wb_B / 100) + (wb_F / 100)) / (C_L - C_D + ((1 - C_L / C_T) ** pr) * C_T)
 
 
 # Calculates the probability of rolling x or more wishes in n rolls
@@ -23,7 +23,7 @@ def PXN(p, n):
     q = 1 - p
     pxn = 0
     for z in range(x, n + 1):
-        pxn += math.comb(n, x) * p ** z * q ** (n - z)
+        pxn += math.comb(n, z) * (p ** z) * (q ** (n - z))
     return pxn
 
 
@@ -31,7 +31,7 @@ optimal_n = 0
 optimal_pxn = 0
 optimal_wb_F = 0
 optimal_wb_B = 0
-for i in range(0, base_bonus_n + 1):
+for i in range(base_bonus_n + 1):
     wb_F = base_wb_F + 10 * i
     wb_B = base_wb_B
 
@@ -45,7 +45,7 @@ for i in range(0, base_bonus_n + 1):
     wb_B += max(i - 15, 0) * 10
 
     p = P(wb_B, wb_F)
-    n = base_n - i
+    n = base_n + base_bonus_n - i
     pxn = PXN(p, n)
     if pxn > optimal_pxn:
         optimal_pxn = pxn
